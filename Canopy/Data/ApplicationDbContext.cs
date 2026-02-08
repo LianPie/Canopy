88e1ff15ac5aae1be279125f4d9e122106307c59
LianPie <SonyWertky@gmail.com>
2026-02-08 14:52:09 +0330

    user groups config

---
+++ Canopy/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Canopy.Data.Configurations;
+using Canopy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Canopy.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Group { get; set; }
+        public DbSet<UserGroup> UserGroup { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // روش ۱: اعمال مستقیم Configuration
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
---
+++ Canopy/Data/Configurations/UserGroupConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Canopy.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Canopy.Data.Configurations
+{
+    public class UserGroupConfiguration
+    {
+        public void Configure(EntityTypeBuilder<UserGroup> builder)
+        {
+            builder.ToTable("UserGroups");
+
+            // Primary Key
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Id)
+                .HasColumnName("UserGroup_Id")
+                .ValueGeneratedOnAdd();
+
+            // Required Fields
+            builder.Property(u => u.UserId)
+                .HasColumnName("User_Id")
+                .IsRequired();
+
+            // Required Fields
+            builder.Property(u => u.GroupId)
+                .HasColumnName("Group_Id")
+                .IsRequired();
+
+            // Required Fields
+            builder.Property(u => u.RoleInGroup)
+                .HasColumnName("Role_In_Group")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            // Required Fields
+            builder.Property(u => u.IsActive)
+                .HasColumnName("Is_Active")
+                .IsRequired();
+
+
+
+        }
+    }
+}
