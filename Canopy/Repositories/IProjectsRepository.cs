3e9bbff9300d68e9a832f1019a46f60d6591db23
LianPie <SonyWertky@gmail.com>
2026-05-08 21:42:02 +0330

    add projects repo

---
+++ Canopy/Repositories/IProjectsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Canopy.Models;
+
+namespace Canopy.Repositories
+{
+    public interface IProjectsRepository
+    {
+        List<Project> GetAllByUser(int userId);
+        Project? GetByIdForUser(int id, int userId);
+        Project Create(Project project);
+        Project Update(Project project);
+        void Delete(Project project);
+    }
+}
